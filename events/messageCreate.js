049a3e877b80e3ad85de8ee520d3521811172a06
cha0ticcoder <120225615+cha0ticcoder@users.noreply.github.com>
2025-02-10 20:31:31 +0100

    converted existing events to esm, updated deploy-commands.js filter for commands, updated index.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,6 @@
-const { Events } = require('discord.js');
+import { Events } from "discord.js";
 
-module.exports = {
+export const event = {
     name: Events.MessageCreate,
     async execute(message) {
 
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const __dirname = import.meta.dirname;
 
 // Require the necessary discord.js classes & bot token
 
-import {Client, Collection, GatewayIntentBits} from "discord.js";
+import { Client, Collection, GatewayIntentBits } from "discord.js";
 const token = process.env.DISCORD_BOT_TOKEN;
 
 // Create a new client instance
@@ -24,8 +24,8 @@ for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') && !file.startsWith('_'));
     for (const file of commandFiles) {
-        const filePath = path.join("file://" + commandsPath, file);
-        const command = await import(filePath);
+        const filePath = path.join(commandsPath, file);
+        const { command } = await import('file://' + filePath);
         // Set a new item in the Collection with the key as the command name and the value as the exported module
         if ('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
@@ -39,8 +39,8 @@ const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js') && !file.startsWith('_'));
 
 for (const file of eventFiles) {
-    const filePath = path.join("file://" + eventsPath, file);
-    const event = await import(filePath);
+    const filePath = path.join(eventsPath, file);
+    const { event } = await import('file://' + filePath);
     if (event.once) {
         client.once(event.name, (...args) => event.execute(...args));
     } else {
