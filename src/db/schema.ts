b3db900fe34fefb582a81007c2d6997647d03bf9
cha0ticcoder <120225615+cha0ticcoder@users.noreply.github.com>
2025-02-12 23:35:50 +0100

    edited schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,38 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
+import {boolean, integer, pgTable, primaryKey, varchar} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-    id: varchar().primaryKey(),
+    id: varchar(),
 });
+
+export const guildsTable = pgTable("guilds", {
+    id: varchar(),
+})
+
+export const availableModulesTable = pgTable("availableModules", {
+    module_id: integer().primaryKey(),
+    module_name: varchar(),
+})
+
+export const modulesTable = pgTable("modules", {
+    module_id: integer().references(() => availableModulesTable.module_id),
+    guild_id: varchar().references(() => guildsTable.id),
+    enabled: boolean().notNull(),
+}, (table) => [
+    primaryKey({ columns: [table.module_id, table.guild_id] }),
+])
+
+export const moduleSettingsTable = pgTable("moduleSettings",{
+    setting_id: integer(),
+    module_id: integer().references(() => availableModulesTable.module_id),
+    guild_id: varchar().references(() => guildsTable.id),
+}, (table) => [
+    primaryKey({ columns: [table.setting_id, table.module_id] }),
+])
+
+export const moduleUserDataTable = pgTable("moduleData",{
+    data_id: integer(),
+    module_id: integer().references(() => availableModulesTable.module_id),
+    guild_id: varchar().references(() => guildsTable.id),
+    user_id: varchar().references(() => usersTable.id),
+
+})
