fd5967363df39a6ab525d3e130030174ff33ca59
cha0ticcoder <cha0ticger@proton.me>
2025-03-13 20:47:00 +0100

    updated schema (schema.ts)

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,32 +1,43 @@
-import {boolean, pgTable, primaryKey, smallserial, varchar} from "drizzle-orm/pg-core";
+import {boolean, index, pgTable, primaryKey, smallserial, text, uniqueIndex, varchar} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable('users', {
-    userId: varchar('guild_id', { length: 19}).primaryKey().notNull(),
+    id: varchar('id', { length: 19}).primaryKey(),
 });
 
 export const guildsTable = pgTable('guilds', {
-    guildId: varchar('guild_id', { length: 19}).primaryKey().notNull(),
+    id: varchar('id', { length: 19}).primaryKey(),
 })
 
 export const modulesTable = pgTable('modules', {
-    moduleId: smallserial('module_id').primaryKey().notNull(),
-})
+    id: smallserial('id').primaryKey(),
+    moduleName: text('module_name'),
+    moduleDescription: text('module_description'),
+});
 
 export const typesTable = pgTable('types', {
-    typeId: smallserial('type_id').primaryKey().notNull(),
-    type: varchar('type').unique().notNull(),
-})
+    id: smallserial('id').primaryKey(),
+    type: text('type').notNull(),
+}, (table) => [
+    uniqueIndex("type_idx").on(table.type)
+])
 
 export const guildModulesTable = pgTable('guild_modules', {
-    guildId: varchar('guild_id', { length: 19}).references(() => guildsTable.guildId),
-    moduleId: smallserial('module_id').references(() => modulesTable.moduleId),
+    guildId: varchar('guild_id', { length: 19}).references(() => guildsTable.id),
+    moduleId: smallserial('module_id').references(() => modulesTable.id),
     isEnabled: boolean('is_enabled'),
 }, (table) => [
-    primaryKey({ columns: [table.guildId, table.moduleId] })
+    primaryKey({ columns: [table.guildId, table.moduleId] }),
+    index("is_enabled_idx").on(table.isEnabled),
 ])
 
 export const moduleDataTable = pgTable('module_data', {
-    moduleId: smallserial('module_id').references(() => modulesTable.moduleId).notNull(),
-    guildId: varchar('guild_id', { length: 19}).references(() => guildsTable.guildId).notNull(),
-    userId: varchar('guild_id').references(() => guildsTable.guildId),
-})
+    moduleId: smallserial('module_id').references(() => modulesTable.id),
+    guildId: varchar('guild_id', { length: 19}).references(() => guildsTable.id),
+    dataName: text('data_name'),
+    userId: varchar('guild_id').references(() => usersTable.id),
+    dataValue: text('data_value'),
+    typeId: smallserial('type_id').references(() => typesTable.id),
+}, (table) => [
+    primaryKey({ columns: [table.moduleId, table.guildId, table.dataName] }),
+    
+]);
